acd20d20e3a5960d24c1dace9d33bbed58f0a2ee
fkadeal <infkadeal@gmail.com>
2021-09-11 23:31:46 -0700

    adding routes hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,10 +14,13 @@ app = Flask(__name__)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
+
+
 @app.route("/hbnb", strict_slashes=False)
-def _hbnb():
+def hbnb():
     """Displays 'HBNB' """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
